9cc7578d3c88a2b51ced1c3a70cf5e6395f66fc4
Raul Sanchez <raulsanchz.h@gmail.com>
2019-12-02 04:16:19 -0600

    Updates

---
+++ precache-manifest.00bd19f084eb7c1aee304418890ad435.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf9d21b41bb0586095092a1a400776e",
+    "url": "/PortafolioFernanda/index.html"
+  },
+  {
+    "revision": "abd82c5ff89298960cb7",
+    "url": "/PortafolioFernanda/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "899e6ca853453194ea07",
+    "url": "/PortafolioFernanda/static/css/main.be785b88.chunk.css"
+  },
+  {
+    "revision": "abd82c5ff89298960cb7",
+    "url": "/PortafolioFernanda/static/js/2.67da3614.chunk.js"
+  },
+  {
+    "revision": "899e6ca853453194ea07",
+    "url": "/PortafolioFernanda/static/js/main.e68a6d08.chunk.js"
+  },
+  {
+    "revision": "7f906c7994e46eb8340b",
+    "url": "/PortafolioFernanda/static/js/runtime-main.0020cb35.js"
+  },
+  {
+    "revision": "6c75a7b392f513f4e5fc4f6e9b0c0399",
+    "url": "/PortafolioFernanda/static/media/BarricaApron.6c75a7b3.jpg"
+  },
+  {
+    "revision": "f0ec9cb6cbcae99c448dc5b321e1d5e7",
+    "url": "/PortafolioFernanda/static/media/BarricaBolsa.f0ec9cb6.jpg"
+  },
+  {
+    "revision": "f2266f03dbebaf2dc374b0bb39f73051",
+    "url": "/PortafolioFernanda/static/media/BarricaBolsaPapel.f2266f03.jpg"
+  },
+  {
+    "revision": "6f7897dda96502e66d0bdc0b377f51ad",
+    "url": "/PortafolioFernanda/static/media/BarricaCaja.6f7897dd.jpg"
+  },
+  {
+    "revision": "b84af89160994a3894b7861d72ca198f",
+    "url": "/PortafolioFernanda/static/media/BarricaInsta1.b84af891.png"
+  },
+  {
+    "revision": "ed2b0aefa9870368dbc7b3182690c390",
+    "url": "/PortafolioFernanda/static/media/BarricaInsta2.ed2b0aef.png"
+  },
+  {
+    "revision": "8b914a146cbb80ab62cf689f5e3967d7",
+    "url": "/PortafolioFernanda/static/media/BarricaTarjeta.8b914a14.jpg"
+  },
+  {
+    "revision": "7e5b55ff52c80da7a0c55bfa5b6617ed",
+    "url": "/PortafolioFernanda/static/media/BaskervilleMTStd-Regular.7e5b55ff.otf"
+  },
+  {
+    "revision": "454bc7942d1121408f1b009e79a0e2d2",
+    "url": "/PortafolioFernanda/static/media/BaskervilleMTStd-SemiBd.454bc794.otf"
+  },
+  {
+    "revision": "65fa8d59b0bee2a067f8ff37491d7d0f",
+    "url": "/PortafolioFernanda/static/media/BloomLogo.65fa8d59.jpg"
+  },
+  {
+    "revision": "d4a8834fa8f57f0929b9f4ef89584361",
+    "url": "/PortafolioFernanda/static/media/Montserrat-Light.d4a8834f.otf"
+  },
+  {
+    "revision": "d815b0a29adf3450c55f56e2fb813be4",
+    "url": "/PortafolioFernanda/static/media/Montserrat-Medium.d815b0a2.otf"
+  },
+  {
+    "revision": "bb3740d350b0186ce32b5678972bf061",
+    "url": "/PortafolioFernanda/static/media/Montserrat-SemiBold.bb3740d3.otf"
+  },
+  {
+    "revision": "670137122ce5e165cac2f0365f127655",
+    "url": "/PortafolioFernanda/static/media/Sporty1.67013712.jpg"
+  },
+  {
+    "revision": "106d6b98f4a998ae8d1add2302d40fb4",
+    "url": "/PortafolioFernanda/static/media/Sporty2.106d6b98.jpg"
+  },
+  {
+    "revision": "eb1cd0de8ba759d438cb470e820f64c6",
+    "url": "/PortafolioFernanda/static/media/Sporty3.eb1cd0de.jpg"
+  },
+  {
+    "revision": "b0b18cfe0c7a323f530453b3cf1e8c58",
+    "url": "/PortafolioFernanda/static/media/Sporty4.b0b18cfe.jpg"
+  },
+  {
+    "revision": "aa14a903466d44fd81c82047a10e861a",
+    "url": "/PortafolioFernanda/static/media/Sporty5.aa14a903.jpg"
+  },
+  {
+    "revision": "e8cc7fd3352f055b905d1cbbd3303b4a",
+    "url": "/PortafolioFernanda/static/media/andrea1-min.e8cc7fd3.jpg"
+  },
+  {
+    "revision": "1db229186987ef618eff6432a14b2e9c",
+    "url": "/PortafolioFernanda/static/media/andrea2-min.1db22918.jpg"
+  },
+  {
+    "revision": "84d296be844ff5717bbf3dafd7c47716",
+    "url": "/PortafolioFernanda/static/media/andrea3-min.84d296be.jpg"
+  },
+  {
+    "revision": "00156a953c89fc8e1613dda3e3afcc44",
+    "url": "/PortafolioFernanda/static/media/andrea5-min.00156a95.jpg"
+  },
+  {
+    "revision": "f092177c58043fdfc8e89a9d3eca8db9",
+    "url": "/PortafolioFernanda/static/media/barricaLogo.f092177c.jpg"
+  },
+  {
+    "revision": "f3856a1cae0918dc78efec7b40098022",
+    "url": "/PortafolioFernanda/static/media/bloomBolsa.f3856a1c.jpg"
+  },
+  {
+    "revision": "70726bc2ad66660f449bd0eb6845793c",
+    "url": "/PortafolioFernanda/static/media/bloomCaja.70726bc2.jpg"
+  },
+  {
+    "revision": "4a95355a9e982a612615f99a564af9ff",
+    "url": "/PortafolioFernanda/static/media/bloomFace.4a95355a.jpg"
+  },
+  {
+    "revision": "ae40396685c762e274cb62afb69c662a",
+    "url": "/PortafolioFernanda/static/media/bloomImg1.ae403966.jpg"
+  },
+  {
+    "revision": "940416e3ff8d64c08e251493f1e32999",
+    "url": "/PortafolioFernanda/static/media/bloomLogo.940416e3.jpg"
+  },
+  {
+    "revision": "59e4109c5c58b4555919b5c134cb1980",
+    "url": "/PortafolioFernanda/static/media/bloomLogoBlack.59e4109c.png"
+  },
+  {
+    "revision": "37a1be5d9c97119d196c3159dc6d223d",
+    "url": "/PortafolioFernanda/static/media/bloomTarjetas.37a1be5d.jpg"
+  },
+  {
+    "revision": "193888cdf70b038c231825ab2b36af65",
+    "url": "/PortafolioFernanda/static/media/conceptoBarrica.193888cd.png"
+  },
+  {
+    "revision": "67d8920d32fe0f82ab0ce7f1731f4e49",
+    "url": "/PortafolioFernanda/static/media/diegoLogo.67d8920d.jpg"
+  },
+  {
+    "revision": "8a967739d7f6bfa972a4bbd19748e129",
+    "url": "/PortafolioFernanda/static/media/dressCover.8a967739.jpg"
+  },
+  {
+    "revision": "28237a527984c49562d723151bc37a91",
+    "url": "/PortafolioFernanda/static/media/dressLogo.28237a52.jpg"
+  },
+  {
+    "revision": "4bd08234068a4fedbb29363e76b273e9",
+    "url": "/PortafolioFernanda/static/media/dressLogo.4bd08234.png"
+  },
+  {
+    "revision": "ddc13eae3e7703c67648a5ba4f07dfb3",
+    "url": "/PortafolioFernanda/static/media/lock1.ddc13eae.jpg"
+  },
+  {
+    "revision": "7495eee37cd4b00a74318e43a6d00f7c",
+    "url": "/PortafolioFernanda/static/media/lock2.7495eee3.jpg"
+  },
+  {
+    "revision": "d2cdef396953eb666e48cf6c16b36d00",
+    "url": "/PortafolioFernanda/static/media/lock3.d2cdef39.jpg"
+  },
+  {
+    "revision": "f943e51e0b4ccf2963f0be0e2bfd0253",
+    "url": "/PortafolioFernanda/static/media/lock4.f943e51e.jpg"
+  },
+  {
+    "revision": "baf76eaef86d2e30cb229911ada4502b",
+    "url": "/PortafolioFernanda/static/media/lock5.baf76eae.jpg"
+  },
+  {
+    "revision": "d9efbff4073bb019d38430f106a2b9cb",
+    "url": "/PortafolioFernanda/static/media/lock6.d9efbff4.jpg"
+  },
+  {
+    "revision": "d02ce56b0f537d41f2422980d26477e6",
+    "url": "/PortafolioFernanda/static/media/lock7.d02ce56b.jpg"
+  },
+  {
+    "revision": "eb0224772bef2ffdf04673e1c198b266",
+    "url": "/PortafolioFernanda/static/media/lock8.eb022477.jpg"
+  },
+  {
+    "revision": "97be18fff150aab0b043d9dac2fb60e1",
+    "url": "/PortafolioFernanda/static/media/lock9.97be18ff.jpg"
+  },
+  {
+    "revision": "a5c7a00f3de2936cc2c11a9b1c205fb0",
+    "url": "/PortafolioFernanda/static/media/logo.a5c7a00f.png"
+  },
+  {
+    "revision": "99b1b01a62beef65776490481b7eeb82",
+    "url": "/PortafolioFernanda/static/media/logoMaderaBarrica.99b1b01a.jpg"
+  },
+  {
+    "revision": "f1f22d4e11e4e462dbe953a6b0cb58c6",
+    "url": "/PortafolioFernanda/static/media/logoMinBarrica.f1f22d4e.png"
+  },
+  {
+    "revision": "c9d5964d81b5c6a33eec1c7ad6e59f13",
+    "url": "/PortafolioFernanda/static/media/luovaBolsa.c9d5964d.jpg"
+  },
+  {
+    "revision": "edc238054f0c7b72f6c3e27615543c34",
+    "url": "/PortafolioFernanda/static/media/luovaEtiqueta.edc23805.jpg"
+  },
+  {
+    "revision": "12b53891499161669bc627a5c27059f7",
+    "url": "/PortafolioFernanda/static/media/luovaGif.12b53891.gif"
+  },
+  {
+    "revision": "84c5a639646a1714cecaac5dc7be09db",
+    "url": "/PortafolioFernanda/static/media/luovaImg1.84c5a639.jpg"
+  },
+  {
+    "revision": "d3629e2e0f6d43454d77beac7f92e853",
+    "url": "/PortafolioFernanda/static/media/luovaImg4.d3629e2e.jpg"
+  },
+  {
+    "revision": "e7042c084b791ffa2e6008ce90446bb3",
+    "url": "/PortafolioFernanda/static/media/luovaImg5.e7042c08.jpg"
+  },
+  {
+    "revision": "e7042c084b791ffa2e6008ce90446bb3",
+    "url": "/PortafolioFernanda/static/media/luovaLogo.e7042c08.jpg"
+  },
+  {
+    "revision": "2a4748aaf61cae3d5e8ca933b4f37d26",
+    "url": "/PortafolioFernanda/static/media/luovaMain.2a4748aa.jpg"
+  },
+  {
+    "revision": "1754c830a8c64b92036feadbf92ff4dc",
+    "url": "/PortafolioFernanda/static/media/luovaPlayera.1754c830.jpg"
+  },
+  {
+    "revision": "b3c3ae792f705939db03e5ff6b455e9c",
+    "url": "/PortafolioFernanda/static/media/luovaTarjeta.b3c3ae79.jpg"
+  },
+  {
+    "revision": "bd311c0c0b5ee5f36b47f3782fb25752",
+    "url": "/PortafolioFernanda/static/media/luovaTarjeta2.bd311c0c.jpg"
+  },
+  {
+    "revision": "aaa47cce523fc3bc2441c0ab6f73ac4a",
+    "url": "/PortafolioFernanda/static/media/luovaTitle.aaa47cce.png"
+  },
+  {
+    "revision": "8ccf392ea3e411c51b48726667fba527",
+    "url": "/PortafolioFernanda/static/media/luovaTitleBlack.8ccf392e.png"
+  },
+  {
+    "revision": "4b952c1d57955caec397e9697b14d396",
+    "url": "/PortafolioFernanda/static/media/lushLogo.4b952c1d.jpg"
+  },
+  {
+    "revision": "390ab84470c7bc213233322499dff6e6",
+    "url": "/PortafolioFernanda/static/media/product1.390ab844.jpg"
+  },
+  {
+    "revision": "b7c90f6736352d3989a11b5a424b4606",
+    "url": "/PortafolioFernanda/static/media/product2.b7c90f67.jpg"
+  },
+  {
+    "revision": "869bd0b9da307ac75ce6ebcdf2e5f1e2",
+    "url": "/PortafolioFernanda/static/media/product3.869bd0b9.jpg"
+  },
+  {
+    "revision": "7aceb11766215efda7246ed9999c022f",
+    "url": "/PortafolioFernanda/static/media/quotes.7aceb117.svg"
+  },
+  {
+    "revision": "8af8e391ea5a1579efdfe85a95e002a5",
+    "url": "/PortafolioFernanda/static/media/restaurante-logo.8af8e391.jpeg"
+  },
+  {
+    "revision": "cbaa36a4e27cc50368ee316fb483a57a",
+    "url": "/PortafolioFernanda/static/media/rightArrow.cbaa36a4.svg"
+  },
+  {
+    "revision": "3bf3d7877a38e8462049dde533866c9a",
+    "url": "/PortafolioFernanda/static/media/sportLogo.3bf3d787.jpg"
+  }
+]);
